d8bdf3bdc789e458727965216e2a5aac6936895e
Jonathan Hou <houj@oxy.edu>
2019-10-08 10:01:01 -0700

    Set up hardcoded list of names and initial server run

--- index.js
+++ index.js
@@ -9,42 +9,44 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+let persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
 
 app.get('/', (req, res)=>{
     res.send('<h1> Hi World </h1>')
 })
 
-app.get('/notes', (req, res)=>{
-  res.json(notes)
+app.get('/api/persons', (req, res)=>{
+  res.json(persons)
 })
 
 
-app.get('/notes/:id',(request, response)=>{
+app.get('/api/persons/:id',(request, response)=>{
     const id = Number(request.params.id)
-    const note = notes.find(note=>note.id===id)
+    const person = persons.find(p=>p.id===id)
 
-    if(note){
-        response.json(note)
+    if(person){
+        response.json(person)
     }else{
         response.status(404).end()
     }
