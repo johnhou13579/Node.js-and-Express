8981098fe0f3f67f5c3e6f16d0d8858b27df0c54
Jonathan Hou <houj@oxy.edu>
2019-10-08 11:18:36 -0700

    Added cors

--- index.js
+++ index.js
@@ -6,8 +6,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
+
 
 let persons = [
   { 
